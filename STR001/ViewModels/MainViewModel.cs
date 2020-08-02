6a860e7777d0e35e47dfd1f73b02168c2c220538
Daymian Tomczyk <daymian.tomczyk@outlook.com>
2020-08-02 19:20:56 -0400

    Initial Commit

---
+++ STR001/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace STR001.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _testBind = "Testing";
+        public string TestBind
+        {
+            get => _testBind;
+            set => Set(ref _testBind, value);
+        }
+
+    }
+}
---
+++ STR001/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonServiceLocator;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace STR001.ViewModels
+{
+    public class ViewModelLocator : ViewModelBase
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            // TODO: Get to this at some point.
+            //if (ViewModelBase.IsInDesignModeStatic)
+            //{
+            //    SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
+            //}
+            //else
+            //{
+            //    SimpleIoc.Default.Register<IDataService, DataService>();         
+            //}
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main
+        {
+            get => SimpleIoc.Default.GetInstance<MainViewModel>();
+        }
+
+    }
+}
